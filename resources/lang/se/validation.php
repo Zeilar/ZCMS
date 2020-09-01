38d6d454b7bd0a70a5e0210b5393939f366bc100
Philip <philip_angelin@hotmail.com>
2020-09-02 01:54:12 +0200

    -

--- resources/lang/se/validation.php
+++ resources/lang/se/validation.php
@@ -113,7 +113,7 @@ return [
     |--------------------------------------------------------------------------
     |
     | Here you may specify custom validation messages for attributes using the
-    | convention "attribute.rule" to name the lines. Thär makes it quick to
+    | convention "attribute.rule" to name the lines. This makes it quick to
     | specify a specific custom language line for a given attribute rule.
     |
     */
@@ -141,5 +141,4 @@ return [
         'password' => 'Lösenordet',
         'password_confirmation' => 'Lösenordet',
     ],
-
 ];
