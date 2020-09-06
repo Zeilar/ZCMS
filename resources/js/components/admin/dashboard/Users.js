a61d51a94b8abde05b34d5940d1b28544f2cf3ea
Philip <philip_angelin@hotmail.com>
2020-09-06 15:32:27 +0200

    Users search

--- resources/js/components/admin/dashboard/Users.js
+++ resources/js/components/admin/dashboard/Users.js
@@ -15,12 +15,12 @@ export default function Users() {
         tableWrapper: {
             'box-shadow': '0 0 15px 0 rgba(0, 0, 0, 0.15)',
             background: 'white',
+            padding: '15px',
         },
         table: {
             border: '1px solid rgb(225, 225, 225)',
             'border-collapse': 'collapse',
-            width: 'calc(100% - 30px)',
-            margin: '15px',
+            width: '100%',
         },
         toolbar: {
             'margin-bottom': '15px',
@@ -34,19 +34,45 @@ export default function Users() {
             width: '25%',
         },
         header: {
-            'margin-right': '10px',
+            'margin-bottom': '15px',
             'font-size': '1.5rem',
             'font-weight': 500,
+        },
+        search: {
+            'margin-left': 'auto',
+        },
+        searchInput: {
+            transition: 'box-shadow 0.05s linear',
+            'border-radius': '5px',
+            'margin-left': '10px',
+            padding: '5px',
+            '&:focus': {
+                'box-shadow': '0 0 0 2px var(--dashboard-primary)',
+            },
         },
     });
     const classes = styles();
 
+    const [searchResults, setSearchResults] = useState([]);
     const [users, setUsers] = useState([]);
+    const searchInput = useRef();
 
     async function getUsers() {
         await fetch('/admin/users/all')
             .then(response => response.json())
             .then(users => setUsers(users));
+    }
+
+    function search() {
+        const results = users.filter(user => {
+            for (const property in user) {
+                if (property === 'created_at' || property === 'updated_at') break;
+                if (String(user[property]).includes(searchInput.current.value)) {
+                    return user;
+                }
+            }
+        });
+        setSearchResults(results);
     }
 
     useEffect(() => {
@@ -57,13 +83,17 @@ export default function Users() {
         <>
             <Sidebar active="users" />
             <div className={classes.wrapper}>
-                <div className={classes.toolbar}>
-                    <h1 className={classes.header}>Users</h1>
-                    <button className="btnDashboard">
-                        Add user
-                    </button>
-                </div>
+                <h1 className={classes.header}>Users</h1>
                 <div className={classes.tableWrapper}>
+                    <div className={classes.toolbar}>
+                        <button className="btnDashboard">
+                            Add user
+                        </button>
+                        <div className={classes.search}>
+                            <span>Search</span>
+                            <input className={classes.searchInput} ref={searchInput} onInput={search} type="text" />
+                        </div>
+                    </div>
                     <table className={classes.table}>
                         <thead className={classes.thead}>
                             <tr className={classes.tr}>
@@ -75,9 +105,13 @@ export default function Users() {
                         </thead>
                         <tbody className={classes.tbody}>
                             {
-                                users.map(({ id, username, email }) => (
-                                    <User id={id} username={username} email={email} setUsers={setUsers} key={id} />
-                                ))
+                                searchResults.length > 0
+                                    ? searchResults.map(({ id, username, email }) => (
+                                        <User id={id} username={username} email={email} setUsers={setUsers} key={id} />
+                                    ))
+                                    : users.map(({ id, username, email }) => (
+                                        <User id={id} username={username} email={email} setUsers={setUsers} key={id} />
+                                    ))
                             }
                         </tbody>
                     </table>
