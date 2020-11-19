d682a80cf2b5318ff904251185f5cc8ab0e19dc3
Philip <philip_angelin@hotmail.com>
2020-11-19 04:14:45 +0100

    Fixed the new role in seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -19,7 +19,7 @@ class UserSeeder extends Seeder
         $superadminRole = Role::where('name', 'superadmin')->first();
         $adminRole = Role::where('name', 'admin')->first();
         $moderatorRole = Role::where('name', 'moderator')->first();
-        $userRole = Role::where('name', 'user')->first();
+        $memberRole = Role::where('name', 'member')->first();
 
         $superadmin = User::create([
             'username' => 'Philip',
@@ -27,31 +27,31 @@ class UserSeeder extends Seeder
             'avatar'   => 'philip.png',
             'password' => Hash::make(env('ADMIN_PASSWORD')),
         ]);
-        $superadmin->roles()->sync([$superadminRole->id, $adminRole->id, $moderatorRole->id, $userRole->id]);
+        $superadmin->roles()->sync([$superadminRole->id, $adminRole->id, $moderatorRole->id, $memberRole->id]);
         
         $admin = User::create([
             'username' => 'Admin',
             'email'    => 'admin@example.com',
             'password' => Hash::make('123'),
         ]);
-        $admin->roles()->sync([$adminRole->id, $moderatorRole->id, $userRole->id]);
+        $admin->roles()->sync([$adminRole->id, $moderatorRole->id, $memberRole->id]);
 
         $moderator = User::create([
             'username' => 'Moderator',
             'email'    => 'moderator@example.com',
             'password' => Hash::make('123'),
         ]);
-        $moderator->roles()->sync([$moderatorRole->id, $userRole->id]);
+        $moderator->roles()->sync([$moderatorRole->id, $memberRole->id]);
 
         $user = User::create([
             'username' => 'User',
             'email'    => 'user@example.com',
             'password' => Hash::make('123'),
         ]);
-        $user->roles()->sync([$userRole->id]);
+        $user->roles()->sync([$memberRole->id]);
 
-        User::factory()->count(20)->create()->each(function($user) use ($userRole) {
-            $user->roles()->attach($userRole);
+        User::factory()->count(20)->create()->each(function($user) use ($memberRole) {
+            $user->roles()->attach($memberRole);
         });
     }
 }
