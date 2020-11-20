25e56052cd6e47388eafb7ae4a2e1bfc21590fbb
Philip <philip_angelin@hotmail.com>
2020-11-20 16:20:26 +0100

    Indents

--- resources/js/components/threads/EditThread.js
+++ resources/js/components/threads/EditThread.js
@@ -85,41 +85,39 @@ export default function EditThread({ thread, refetch }) {
             <form className={classnames(classes.wrapper, 'row center-children mb-2')} onSubmit={submit}>
                 {
                     editing
-                        ? (
-                            <>
-                                <input 
-                                    className={classnames(classes.input, 'w-100')} value={input}
-                                    onChange={e => setInput(e.target.value)} placeholder="Title"
-                                />
-                                {
-                                    canRemove() &&
-                                        <button
-                                            className={classnames(classes.button, 'btn btn-outline btn-danger ml-2')}
-                                            onClick={remove} type="button"
-                                        >
-                                            {deleting ? <Icon path={mdiLoading} spin={1} /> : <Icon path={mdiTrashCanOutline} />}
-                                        </button>
-                                }
-                                {
-                                    canLock() &&
-                                        <button 
-                                            className={classnames(classes.button, 'btn btn-outline mx-2 btn-dark')}
-                                            onClick={() => setLocked(p => p ? 0 : 1)} type="button"
-                                        >
-                                            <Icon path={locked ? mdiLockOutline : mdiLockOpenOutline} />
-                                        </button>
-                                }
-                                <button className={classnames(classes.button, 'btn btn-outline ml-2')} onClick={submit} disabled={submitting}>
-                                    {submitting ? <Icon path={mdiLoading} spin={1} /> : <Icon path={mdiCheck} />}
-                                </button>
-                                <button
-                                    className={classnames(classes.button, 'btn btn-outline ml-2 btn-dark')}
-                                    onClick={() => setEditing(false)} type="button"
-                                >
-                                    <Icon path={mdiClose} />
-                                </button>
-                            </>
-                        )
+                        ? <>
+                            <input 
+                                className={classnames(classes.input, 'w-100')} value={input}
+                                onChange={e => setInput(e.target.value)} placeholder="Title"
+                            />
+                            {
+                                canRemove() &&
+                                    <button
+                                        className={classnames(classes.button, 'btn btn-outline btn-danger ml-2')}
+                                        onClick={remove} type="button"
+                                    >
+                                        {deleting ? <Icon path={mdiLoading} spin={1} /> : <Icon path={mdiTrashCanOutline} />}
+                                    </button>
+                            }
+                            {
+                                canLock() &&
+                                    <button 
+                                        className={classnames(classes.button, 'btn btn-outline mx-2 btn-dark')}
+                                        onClick={() => setLocked(p => p ? 0 : 1)} type="button"
+                                    >
+                                        <Icon path={locked ? mdiLockOutline : mdiLockOpenOutline} />
+                                    </button>
+                            }
+                            <button className={classnames(classes.button, 'btn btn-outline ml-2')} onClick={submit} disabled={submitting}>
+                                {submitting ? <Icon path={mdiLoading} spin={1} /> : <Icon path={mdiCheck} />}
+                            </button>
+                            <button
+                                className={classnames(classes.button, 'btn btn-outline ml-2 btn-dark')}
+                                onClick={() => setEditing(false)} type="button"
+                            >
+                                <Icon path={mdiClose} />
+                            </button>
+                        </>
                         : (
                             <button className={classnames(classes.button, 'btn btn-outline ml-auto btn-dark')} onClick={() => setEditing(true)} type="button">
                                 <Icon path={mdiCogOutline} />
