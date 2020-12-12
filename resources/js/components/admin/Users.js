1d6fa06520c42565c999a583b9448cbac74c63f8
Philip <philip_angelin@hotmail.com>
2020-12-12 14:57:29 +0100

    Första action färdig (delete user)

--- resources/js/components/admin/Users.js
+++ resources/js/components/admin/Users.js
@@ -1,9 +1,12 @@
 import React, { useState, useContext, useEffect } from 'react';
 import { FeedbackModalContext } from '../../contexts';
+import { mdiPencil, mdiTrashCan } from '@mdi/js';
 import { createUseStyles } from 'react-jss';
 import { MaterialTable } from '../misc';
 import { Http } from '../../classes';
 import classnames from 'classnames';
+import Icon from '@mdi/react';
+import { errorCodeHandler } from '../../functions/helpers';
 
 export default function Users() {
     const { setMessage } = useContext(FeedbackModalContext);
@@ -18,6 +21,11 @@ export default function Users() {
         }
     }, []);
 
+    async function deleteUser(id) {
+        const { code } = await Http.delete(`admin/users/${id}`);
+        errorCodeHandler(code, message => setMessage(message), () => setUsers(p => p.filter(user => user.id !== id)));
+    }
+
     return (
         <div>
             <MaterialTable
@@ -26,10 +34,29 @@ export default function Users() {
                     { title: 'Email', field: 'email' },
                 ]}
                 options={{
-                    selection: true,
+                    // selection: true,
                     pageSize: 10,
                 }}
                 data={users}
+                actions={[
+                    {
+                        icon: () => <Icon path={mdiPencil} />,
+                        tooltip: 'Edit user',
+                        onClick: (e, rowData) => {
+                            
+                        },
+                    },
+                    {
+                        icon: () => <Icon className={classnames('color-danger')} path={mdiTrashCan} />,
+                        tooltip: 'Delete user',
+                        onClick: (e, rowData) => {
+                            if (!confirm(`Are you sure you want to delete user ${rowData.username}?`)) {
+                                return;
+                            }
+                            deleteUser(rowData.id);
+                        },
+                    },
+                ]}
                 title="Users"
             />
         </div>
