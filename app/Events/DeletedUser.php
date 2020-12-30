babefd70771865763865fa403721121ce0ceede5
Philip <philip_angelin@hotmail.com>
2020-12-30 17:44:42 +0100

    Bugfixes

---
+++ app/Events/DeletedUser.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Events;
+
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
+
+class DeletedUser implements ShouldBroadcastNow
+{
+    use Dispatchable, SerializesModels;
+
+    public function broadcastOn()
+    {
+        return new Channel('admin-statistics');
+    }
+}
